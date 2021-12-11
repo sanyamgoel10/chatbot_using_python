b5c8b937a27d3e0bb032f30343977142e7164110
Sanyam <sanyamgoel10@gmail.com>
2021-12-11 12:55:44 +0530

    1255 11-12-2021

--- chatbot.py
+++ chatbot.py
@@ -2,35 +2,36 @@ import bs4 as bs
 import urllib.request
 import re
 import nltk
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
+# function to answer a query
 def querries(query):
-    cat_data = urllib.request.urlopen("https://en.wikipedia.org/wiki/India").read()
-    # cat_data = urllib.request.urlopen("https://drive.google.com/file/d/1auHIrTRMmqnvAVzYwpN18ksxtSNHjXW7/view").read()
-    cat_data_paragraphs = bs.BeautifulSoup(cat_data, 'lxml').find_all('p')
-    cat_text = ''
-    for p in cat_data_paragraphs:
-        cat_text += p.text.lower()
-    # print(cat_text)
+    gett_data = urllib.request.urlopen("https://en.wikipedia.org/wiki/India").read()
+    gett_data_paragraphs = bs.BeautifulSoup(gett_data, 'lxml').find_all('p')
+    gett_text = ''
+    for p in gett_data_paragraphs:
+        gett_text += p.text.lower()
+    # print(gett_text)
 
-    cat_text = re.sub(r'\s+', ' ', re.sub(r'\[[0-9]*]', ' ', cat_text))
+    gett_text = re.sub(r'\s+', ' ', re.sub(r'\[[0-9]*]', ' ', gett_text))
 
     nltk.download('punkt')
-    cat_sentences = nltk.sent_tokenize(cat_text)
+    gett_sentences = nltk.sent_tokenize(gett_text)
 
-    from sklearn.metrics.pairwise import cosine_similarity
-    from sklearn.feature_extraction.text import TfidfVectorizer
+
 
     def chatbot_answer(user_query):
         # Append the query to the sentences list
-        cat_sentences.append(user_query)
+        gett_sentences.append(user_query)
         # Create the sentences vector based on the list
         vectorizer = TfidfVectorizer()
-        sentences_vectors = vectorizer.fit_transform(cat_sentences)
+        sentences_vectors = vectorizer.fit_transform(gett_sentences)
 
         # Measure the cosine similarity and take the second closest index because the first index is the user query
         vector_values = cosine_similarity(sentences_vectors[-1], sentences_vectors)
-        answer = cat_sentences[vector_values.argsort()[0][-2]]
+        answer = gett_sentences[vector_values.argsort()[0][-2]]
         # Final check to make sure there are result present. If all the result are 0, means the text input by us are not captured in the corpus
         input_check = vector_values.flatten()
         input_check.sort()
@@ -40,12 +41,11 @@ def querries(query):
         else:
             return answer
 
-    # print("Hello, I am the Cat Chatbot. What is your meow questions?:")
     while True:
         # query = input().lower()
         if query not in ['bye', 'good bye', 'take care']:
             lmnop = chatbot_answer(query)
-            cat_sentences.remove(query)
+            gett_sentences.remove(query)
             return lmnop
         else:
             return "See You Again"
